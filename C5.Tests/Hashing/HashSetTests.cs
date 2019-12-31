4901d561dda3ee6385aa0f366ff34b51aee09904
Shad Storhaug <shad@shadstorhaug.com>
2020-01-01 00:20:22 +0700

    Added SCG.ISet<T> to HashSet<T> (#53)

---
+++ C5.Tests/Hashing/HashSetTests.cs
@@ -0,0 +1,18 @@
+﻿// This file is part of the C5 Generic Collection Library for C# and CLI
+// See https://github.com/sestoft/C5/blob/master/LICENSE.txt for licensing details.
+
+using C5.Tests.Templates.Set;
+using System.Collections.Generic;
+
+namespace C5.Tests.Hashing
+{
+    public class SCG_ISet : SCG_ISetBase
+    {
+        public override ISet<string> CreateSet(IEqualityComparer<string> equalityComparer, params string[] values)
+        {
+            var set = new HashSet<string>(equalityComparer);
+            set.UnionWith(values);
+            return set;
+        }
+    }
+}
